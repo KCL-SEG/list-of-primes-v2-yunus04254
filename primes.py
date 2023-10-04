bd4056a51d5d6e75b495332fe0e06f9054833fdf
Yunus Sufian <yunus.sufian@kcl.ac.uk>
2023-10-04 12:02:33 +0100

    Add error and solution

--- primes.py
+++ primes.py
@@ -3,4 +3,18 @@
 
 def primes(number_of_primes):
     list = []
+    currentNum = 2
+
+    if number_of_primes >= 1:
+        while len(list) != number_of_primes:
+            isPrime = True
+            for i in range(2, currentNum):
+                if (currentNum % i == 0) and (currentNum != 2):
+                    isPrime = False
+                    break
+            if isPrime:
+                list.append(currentNum)
+            currentNum += 1
+    elif number_of_primes < 1:
+        raise ValueError
     return list
